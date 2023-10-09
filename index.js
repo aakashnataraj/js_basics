c0d527767c4f17109c43259a5921da477e9d1bad
aakashnataraj <aakashnataraj345@gmail.com>
2023-10-09 22:14:01 +0530

    js.practice

--- index.js
+++ index.js
@@ -1,8 +1,70 @@
-let place = {
-  FirstName: "Aakash",
-  Age: 45,
-  gender: "Male",
-  place: "Erumapatti"
-};
+function add(number1,number2){
+    console.log(number1 + number2)
 
-console.log(place[length]);
+}   
+
+add(60 , 1000)
+
+
+function number(value1 , value2){
+    console.log(value1 + value2)
+}
+
+number('Cloth' , 45)
+
+
+function myfunction(para1,para2){
+    console.log(para1 + para2);
+}
+   myfunction(15, 4)
+
+
+function variable(var1,var2){
+    console.log(var1 + var2);
+}
+
+variable(25 , 123450)
+
+
+function user(age1,age2){
+    console.log(age1 + age2);
+}
+
+user(25 , 35)
+
+
+
+function name(name1 , name2 , name3){
+    console.log(name1 + name2 + name3);
+}
+
+name ('vasu ',' Deva',' Hari')
+
+
+operators//
+
+let x = 15;
+
+x = x + 23;
+
+console.log(x);
+
+
+var y = 150;
+
+y = y + 235;
+
+console.log(y);
+
+let number1 = 5;
+let number2 = 14;
+
+console.log(--number1);
+
+
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 % number2);
+console.log(number1 / number2);
